126435f22ed1ebfecd23ddb7b1b543b0d652c9ef
NedoCPP <formassivspam@mail.ru>
2017-06-04 17:34:17 +0100

    +BishopChess functions (without step)

--- bishopchess.h
+++ bishopchess.h
@@ -1,4 +1,21 @@
 #ifndef BISHOPCHESS_H
 #define BISHOPCHESS_H
 
+#include "basechess.h"
+#include <string>
+
+struct BishopChess:public BaseChess
+{
+    std::vector<std::pair<int,int>> movesBishop;//possible moves
+
+       bool step(int x, int y, BoardChess*) override;
+       BishopChess(int,int, std::string,BoardChess*,bool);
+
+       void possibleMoves(BoardChess*);
+       ~BishopChess(){};
+};
+
+
+
+
 #endif // BISHOPCHESS_H
